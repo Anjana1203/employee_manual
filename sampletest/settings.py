217753cafa0b6ce860a494313d942158396c035b
Anjana P <anjanap525@gmail.com>
2020-01-22 01:34:26 +0530

    Updated settings

--- sampletest/settings.py
+++ sampletest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sampletest.employee',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'sampletest.urls'
 
 TEMPLATES = [
     {
