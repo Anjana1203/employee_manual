d92c5bbba6fc4749484ebd44cd55a8a00a2684ae
Anjana P <anjanap525@gmail.com>
2020-01-22 00:36:26 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sampletest.wsgi --log-file -
---
+++ sampletest/urls.py
@@ -0,0 +1,31 @@
+"""sampletest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from employee.views import EmployeeView, Agent, login_page, logout_page, employee_details
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('emp/', EmployeeView.as_view(), name='emp'),
+    path('agent/', Agent.as_view(), name='agent'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
+    re_path(r'^emp_view/(?P<emp_id>\d+)/', employee_details, name='emp_view'),
+
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
