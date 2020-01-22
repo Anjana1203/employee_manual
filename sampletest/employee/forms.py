ca4728ac6a6a30cfe5b195cfbc71a808a2771c95
Anjana P <anjanap525@gmail.com>
2020-01-23 01:53:15 +0530

    new UI changes

--- sampletest/employee/forms.py
+++ sampletest/employee/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
-
 from .models import GENDER
 
 class LoginForm(forms.Form):
-	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username.'
-                                                                                                         '','required':'required', 'autofocus': 'autofocus'}))
+	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'
+                                                                                                         '','required':'required', 'autofocus': 'autofocus', 'autocomplete': 'off' }))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 	def clean_username(self):
@@ -21,10 +20,10 @@ class LoginForm(forms.Form):
 
 
 class EmployeeForm(forms.Form):
-	employee_fname = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_lname= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_email= forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_address= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_phone= forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_gender= forms.ChoiceField(choices=GENDER ,widget=forms.Select(attrs={'class': 'form-control', 'placeholder': ''}))
-	employee_image= forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': ''}))
+	employee_fname = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_lname= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_email= forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_address= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_phone= forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_gender= forms.ChoiceField(choices=GENDER ,widget=forms.Select(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
+	employee_image= forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'form-control', 'placeholder': '', 'autocomplete': 'off'}))
